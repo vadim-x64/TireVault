a9918680b90c8d4a25df5cf3cff0543e281b33b9
Vadym <vadim.rolex.2005@gmail.com>
2026-02-24 15:05:07 +0200

    chore: setup empty project.

---
+++ src/main/java/course/project/ua/tirevault/ServletInitializer.java
@@ -0,0 +1,13 @@
+package course.project.ua.tirevault;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TireVaultApplication.class);
+    }
+
+}
---
+++ src/main/java/course/project/ua/tirevault/TireVaultApplication.java
@@ -0,0 +1,11 @@
+package course.project.ua.tirevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TireVaultApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TireVaultApplication.class, args);
+    }
+}
