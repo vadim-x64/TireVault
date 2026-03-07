0dd2d92994564fcc174fcae1463dba808da2c2dd
Vadym <vadim.rolex.2005@gmail.com>
2026-03-07 15:41:59 +0200

    update: created new and fixed old pages.

--- src/main/java/course/project/ua/tirevault/Controllers/HomeController.java
+++ src/main/java/course/project/ua/tirevault/Controllers/HomeController.java
@@ -11,4 +11,34 @@ public class HomeController {
         model.addAttribute("page", "home");
         return "index";
     }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("page", "about");
+        return "index";
+    }
+
+    @GetMapping("/contacts")
+    public String contacts(Model model) {
+        model.addAttribute("page", "contacts");
+        return "index";
+    }
+
+    @GetMapping("/delivery")
+    public String delivery(Model model) {
+        model.addAttribute("page", "delivery");
+        return "index";
+    }
+
+    @GetMapping("/gallery")
+    public String gallery(Model model) {
+        model.addAttribute("page", "gallery");
+        return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog(Model model) {
+        model.addAttribute("page", "blog");
+        return "index";
+    }
 }
